a2ab47352bc73bd223cbc58d7d2006772eff827c
Mathew Scott-Halliday <s214285@students.aie.edu.au>
2022-02-17 20:00:52 +1030

    adding attacking and defending

--- Program.cs
+++ Program.cs
@@ -29,11 +29,24 @@ namespace ConsoleApp4
                 {
                     curMonster = SpawnMonster(curPlayer.Level);
                     Console.WriteLine(curMonster.ID);
-                    Console.WriteLine("A monster is before you, \n" +
-                        "Health: {0} \n" + "Damage: {1} \n", curMonster.Health, curMonster.Damage);
-                    Console.ReadLine();
+
+                }
+                if(!curMonster.Dead || curMonster != null)
+                {
+                    GameLoop(curPlayer, curMonster);
                 }
             }
+        }
+        public static void GameLoop(Player player, Monster monster)
+        {
+            Console.WriteLine("A monster is before you, \n" +
+                "Health: {0} \n" + "Damage: {1} \n", monster.Health, monster.Damage);
+            Console.WriteLine("Press A to attack or D to block");
+            if(Console.ReadKey().Key == ConsoleKey.A)
+            {
+                
+            }
+            Console.ReadLine();
         }
         public static Player SpawnPlayer()
         {
@@ -42,6 +55,11 @@ namespace ConsoleApp4
                 Health = 100,
                 Damaage = 5,
                 Level = 1,
+                BlockChance = 10,
+                CritChance = 5,
+                CritDamage = 1.2f,
+                
+                XP = 0,
                 Dead = false,
             };
             // Creates player obviously
@@ -54,6 +72,8 @@ namespace ConsoleApp4
             {
                 Health = 100 * (playerLevel * 0.5),
                 Damage = 5,
+                DodgeChance = 5,
+                BlockChance = 0,
                 XPOnKill = (float)RandomExtension.NextDoubleRange(random, 0, 3.5),
                 ID = random.Next(0, 999),
                 Dead = false
@@ -74,6 +94,8 @@ namespace ConsoleApp4
         public float Damage;
         public float XPOnKill;
         public bool Dead;
+        public int DodgeChance;
+        public int BlockChance;
         public int ID;
     }
     public class Player
@@ -81,7 +103,11 @@ namespace ConsoleApp4
         public int Health;
         public int Damaage;
         public int Level;
+        public int BlockChance;
+        public int CritChance;
+        public float CritDamage;
         public float XPToLevelUp;
+        public float XP;
         public bool Dead;
     }
 }
